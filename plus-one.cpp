2b2308ffdb92bfb3faa28e63c6a973ebd8bcf6f9
Mohd sharib <mohdsharib7517@gmail.com>
2022-09-26 01:22:13 +0530

    first commit

---
+++ plus-one.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        for (int i = digits.size() - 1; i >= 0; --i)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+int main()
+{
+}
