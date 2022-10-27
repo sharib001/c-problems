80626a5a16eb48650b2c460c0fc489eb1c5192c1
Mohd sharib <mohdsharib7517@gmail.com>
2022-10-27 08:49:40 +0530

    27-10-2022

---
+++ Search-inRotated-Array.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int low = 0, high = nums.size() - 1, mid;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[low] <= nums[mid])
+            {
+                if (nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else
+            {
+                if (target >= nums[mid] && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+   
+    return 0;
+}
