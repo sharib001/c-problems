3a8b1aeca71a83fa0afe4151d61984bb0a162460
Mohd sharib <mohdsharib7517@gmail.com>
2022-11-30 01:38:25 +0530

    30-11-2022

---
+++ Operation-to-array.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] = 2*nums[i];
+                nums[i+1] = 0;
+            }
+        }
+        int j = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+        return nums;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
